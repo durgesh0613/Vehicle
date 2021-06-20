607859ba839b774a066632ded83cfced723153ba
wkdankit <ankit.pattanayak97@gmail.com>
2021-06-19 18:57:50 -0700

    DataUtils error fix

--- app/src/main/java/com/example/vehicle/VehicleDetailFragment.java
+++ app/src/main/java/com/example/vehicle/VehicleDetailFragment.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-import com.example.vehicle.content.DataUtils;
 import com.example.vehicle.tasks.GetVehicleImage;
 
 import java.util.HashMap;
